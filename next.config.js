137fc36b79b64d4256a936c0e4bb8d91a9fd0e2f
mikuta1307 <akihito.kataoka.0703@gmail.com>
2025-05-20 20:11:54 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-// next.config.js
 module.exports = {
-  reactStrictMode: true,
-  // 画像の最適化設定
+  // GitHub Pages用の設定
+  basePath: process.env.NODE_ENV === 'production' ? '/skindiving-starter' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/skindiving-starter/' : '',
+  // 画像最適化の無効化（GitHub Pages対応）
   images: {
-    domains: ['localhost'],
-  },
-};
+    unoptimized: true
+  }
+}
