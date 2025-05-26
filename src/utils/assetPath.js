57b4f2ebb1a036f4a2c0a02e8276a50969cd7f51
mikuta1307 <akihito.kataoka.0703@gmail.com>
2025-05-26 14:16:46 +0900

    Update assetPath.js

--- src/utils/assetPath.js
+++ src/utils/assetPath.js
@@ -1,6 +1,7 @@
 // src/utils/assetPath.js
 export const getAssetPath = (path) => {
   // 本番環境（GitHub Pages）ではbasePathを設定
-  const basePath = process.env.NODE_ENV === 'production' ? '/skin-diving' : '';
+  //const basePath = process.env.NODE_ENV === 'production' ? '/skin-diving' : '';
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
   return `${basePath}${path}`;
 };
